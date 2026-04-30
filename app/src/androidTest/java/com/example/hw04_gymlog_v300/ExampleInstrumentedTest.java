7397590bac3479660b0a280eeb8934e2958d2c2c
JTzeng1 <jtzeng@csumb.edu>
2026-04-29 22:48:21 -0700

    fixed stuff

--- app/src/androidTest/java/com/example/hw04_gymlog_v300/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/hw04_gymlog_v300/ExampleInstrumentedTest.java
@@ -1,10 +1,19 @@
 package com.example.hw04_gymlog_v300;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +26,29 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.hw04_gymlog_v300", appContext.getPackageName());
+
+
     }
+    @Test
+    public void testExample() {
+        // Find
+        onView(withId(R.id.view_id))
+
+                // Act
+                .perform(typeText("Hello"), click());
+
+        // Check
+        onView(withId(R.id.view_id))
+                .check(matches(withText("Hello")));
+
+    }
+
 }
